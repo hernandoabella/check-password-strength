8fa38565c9640cf8ee88325fd7cb034c71298402
Hernando Abella <hernandoabella@gmail.com>
2023-08-11 00:19:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,56 @@
 (() => {
-  /**
-   * Parse a password string into a numeric value.
-   *
-   * @param {string} password
-   * @return {number}
-   */
-  let evaluatePassword = (password) => {
-    let score = 0;
-    score = password.length;
-    score = password.match(/[!]/gim)
-      ? score + password.match(/[!]/gim).length * 3
-      : score;
-    score = password.match(/[A-Z]/gm) ? score + 3 : score;
-    score = password.match(/[0-9]/gim) ? score + 3 : score;
-    return score;
-  };
-  /**
-   * Convert a numeric score into an object of 'DOM update' data.
-   *
-   * @param {number} score
-   * @return {Object}
-   */
-  let scoreToData = (score) => {
-    if (score >= 30) {
-      return {
-        width: "100%",
-        color: "#26de81",
-        label: "Strong",
-      };
-    } else if (score >= 20) {
-      return {
-        width: "66%",
-        color: "#fd9644",
-        label: "Medium",
-      };
-    } else {
-      return {
-        width: "33%",
-        color: "#fc5c65",
-        label: "Weak",
-      };
-    }
-  };
-  window.addEventListener("DOMContentLoaded", () => {
-    // Get element refs.
-    let p = document.querySelector(".js--password");
-    let b = document.querySelector(".js--password-bar");
-    let t = document.querySelector(".js--password-text");
-    // Listen for updates to password field.
-    p.addEventListener("input", () => {
-      // Convert current value to data.
-      let data = scoreToData(evaluatePassword(p.value));
-      // Update DOM.
-      b.style.width = data.width;
-      b.style.background = data.color;
-      t.innerText = data.label;
+    const STRONG_THRESHOLD = 30;
+    const MEDIUM_THRESHOLD = 20;
+    const CHAR_OCCURRENCE_SCORE = 3;
+  
+    const countCharacterOccurrences = (password, pattern) => {
+      const matches = password.match(pattern);
+      return matches ? matches.length : 0;
+    };
+  
+    const evaluatePassword = (password) => {
+      let score = password.length;
+      score += countCharacterOccurrences(password, /[!]/gim) * CHAR_OCCURRENCE_SCORE;
+      score += password.match(/[A-Z]/gm) ? CHAR_OCCURRENCE_SCORE : 0;
+      score += password.match(/[0-9]/gim) ? CHAR_OCCURRENCE_SCORE : 0;
+      return score;
+    };
+  
+    const scoreToData = (score) => {
+      if (score >= STRONG_THRESHOLD) {
+        return {
+          width: "100%",
+          color: "#26de81",
+          label: "Strong",
+        };
+      } else if (score >= MEDIUM_THRESHOLD) {
+        return {
+          width: "66%",
+          color: "#fd9644",
+          label: "Medium",
+        };
+      } else {
+        return {
+          width: "33%",
+          color: "#fc5c65",
+          label: "Weak",
+        };
+      }
+    };
+  
+    window.addEventListener("DOMContentLoaded", () => {
+      const passwordInput = document.querySelector(".js--password");
+      const progressBar = document.querySelector(".js--password-bar");
+      const statusText = document.querySelector(".js--password-text");
+  
+      passwordInput.addEventListener("input", () => {
+        const password = passwordInput.value;
+        const data = scoreToData(evaluatePassword(password));
+  
+        progressBar.style.width = data.width;
+        progressBar.style.background = data.color;
+        statusText.innerText = data.label;
+      });
     });
-  });
-})();
+  })();
+  
