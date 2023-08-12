3955fe54204d5868d3789b70244877c70691060e
Hernando Abella <hernandoabella@gmail.com>
2023-08-12 17:44:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,71 @@
 (() => {
-    const STRONG_THRESHOLD = 30;
-    const MEDIUM_THRESHOLD = 20;
-    const CHAR_OCCURRENCE_SCORE = 3;
-  
-    const countCharacterOccurrences = (password, pattern) => {
-      const matches = password.match(pattern);
-      return matches ? matches.length : 0;
-    };
-  
-    const evaluatePassword = (password) => {
-      let score = password.length;
-      score += countCharacterOccurrences(password, /[!]/gim) * CHAR_OCCURRENCE_SCORE;
-      score += password.match(/[A-Z]/gm) ? CHAR_OCCURRENCE_SCORE : 0;
-      score += password.match(/[0-9]/gim) ? CHAR_OCCURRENCE_SCORE : 0;
-      return score;
-    };
-  
-    const scoreToData = (score) => {
-      if (score >= STRONG_THRESHOLD) {
-        return {
-          width: "100%",
-          color: "#26de81",
-          label: "Fuerte",
-        };
-      } else if (score >= MEDIUM_THRESHOLD) {
-        return {
-          width: "66%",
-          color: "#fd9644",
-          label: "Medio",
-        };
-      } else {
-        return {
-          width: "33%",
-          color: "#fc5c65",
-          label: "Débil",
-        };
-      }
-    };
-  
-    window.addEventListener("DOMContentLoaded", () => {
-      const passwordInput = document.querySelector(".js--password");
-      const progressBar = document.querySelector(".js--password-bar");
-      const statusText = document.querySelector(".js--password-text");
-    
-      passwordInput.addEventListener("input", () => {
-        const password = passwordInput.value.trim(); // Elimina espacios en blanco al principio y al final
-    
-        if (password === "") {
-          progressBar.style.width = "0";
-          progressBar.style.background = "transparent";
-          statusText.innerText = "🔐";
-          return;
-        }
-    
-        const data = scoreToData(evaluatePassword(password));
-    
-        progressBar.style.width = data.width;
-        progressBar.style.background = data.color;
-        statusText.innerText = data.label;
-      });
+  const STRONG_THRESHOLD = 30;
+  const MEDIUM_THRESHOLD = 20;
+  const CHAR_OCCURRENCE_SCORE = 3;
+
+  const countCharacterOccurrences = (password, pattern) => {
+    const matches = password.match(pattern);
+    return matches ? matches.length : 0;
+  };
+
+  const evaluatePassword = (password) => {
+    let score = password.length;
+    score += countCharacterOccurrences(password, /[!]/gim) * CHAR_OCCURRENCE_SCORE;
+    score += password.match(/[A-Z]/gm) ? CHAR_OCCURRENCE_SCORE : 0;
+    score += password.match(/[0-9]/gim) ? CHAR_OCCURRENCE_SCORE : 0;
+    return score;
+  };
+
+  const scoreToData = (score) => {
+    if (score >= STRONG_THRESHOLD) {
+      return {
+        width: "100%",
+        color: "#26de81",
+        label: "Fuerte",
+      };
+    } else if (score >= MEDIUM_THRESHOLD) {
+      return {
+        width: "66%",
+        color: "#fd9644",
+        label: "Medio",
+      };
+    } else {
+      return {
+        width: "33%",
+        color: "#fc5c65",
+        label: "Débil",
+      };
+    }
+  };
+
+  window.addEventListener("DOMContentLoaded", () => {
+    const passwordInput = document.querySelector(".js--password");
+    const progressBar = document.querySelector(".js--password-bar");
+    const statusText = document.querySelector(".js--password-text");
+    const togglePasswordBtn = document.getElementById("togglePassword");
+
+    togglePasswordBtn.addEventListener("click", () => {
+      const type = passwordInput.getAttribute("type");
+      passwordInput.setAttribute("type", type === "password" ? "text" : "password");
+      togglePasswordBtn.querySelector("i").classList.toggle("fa-eye");
+      togglePasswordBtn.querySelector("i").classList.toggle("fa-eye-slash");
     });
-    
-  })();
-  
+
+    passwordInput.addEventListener("input", () => {
+      const password = passwordInput.value.trim();
+
+      if (password === "") {
+        progressBar.style.width = "0";
+        progressBar.style.background = "transparent";
+        statusText.innerText = "🔐";
+        return;
+      }
+
+      const data = scoreToData(evaluatePassword(password));
+
+      progressBar.style.width = data.width;
+      progressBar.style.background = data.color;
+      statusText.innerText = data.label;
+    });
+  });
+})();
